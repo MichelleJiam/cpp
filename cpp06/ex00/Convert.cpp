f5c2aeffe6666d2cf3c4173e2e6a92f9a6ce6cc0
mjiam <mjiam@student.codam.nl>
2020-09-09 17:03:41 +0200

    cpp06/ex00 completed

--- cpp06/ex00/Convert.cpp
+++ cpp06/ex00/Convert.cpp
@@ -6,31 +6,113 @@
 /*   By: mjiam <mjiam@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/07 17:50:15 by mjiam         #+#    #+#                 */
-/*   Updated: 2020/09/08 18:18:56 by mjiam         ########   odam.nl         */
+/*   Updated: 2020/09/09 16:53:04 by mjiam         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Convert.hpp"
 
-Convert::Convert(std::string &input) : _dataType(kInvalid), _flag(kNone) {
-    // _types[0] = "%d";
-    // _types[1] = "%f";
-    // _types[2] = "%lf";
-    // for (int i = 0; i < 3; i++)
-    try {
-        this->_dataType = detectType(input);
-        if (this->_dataType == kInvalid)
-            throw std::invalid_argument("Invalid type");
-        convertData(input);
-        printData();
+Convert::Convert(void) : _dataType(kInvalid), _flag(kNone) {
+    return;
+}
+
+Convert::Convert(Convert const &other) {
+    *this = other;
+    return;
+}
+
+Convert &Convert::operator=(Convert const &other) {
+    if (this != &other) {
+        this->_dataType = other._dataType;
+        this->_flag = other._flag;
+        for (int i = 0; i < 4; i++)
+            this->_exceptions[i] = other._exceptions[i];
+        this->_char = other._char;
+        this->_int = other._int;
+        this->_float = other._float;
+        this->_double = other._double;
     }
-    catch (std::invalid_argument &e) {
-        std::cerr << e.what() << std::endl;
+    return *this;
+}
+
+Convert::~Convert(void) {
+    return;
+}
+
+void    Convert::convertData(std::string &input) {
+    this->_dataType = detectType(input);
+    if (this->_dataType == kInvalid)
+        throw std::invalid_argument("Invalid type");
+    if (this->_dataType == kChar)
+        convertChar();
+    else {
+        checkOverflow(input);
+        sscanf(input.c_str(), "%lf", &this->_double);
+        if (this->_flag == kNone)
+            convertDigit();
+        else
+            this->_float = static_cast<float>(this->_double);
     }
     return;
 }
 
-Convert::~Convert(void) {
+void    Convert::printData(void) const {
+    std::cout << "char: ";
+    if (this->_exceptions[kChar].empty())
+        std::cout << this->_char << std::endl;
+    else
+        std::cout << this->_exceptions[kChar] << std::endl;
+    std::cout << "int: ";
+    if (this->_exceptions[kInt].empty())
+        std::cout << this->_int << std::endl;
+    else
+        std::cout << this->_exceptions[kInt] << std::endl;
+    std::cout << "float: ";
+    if (this->_exceptions[kFloat].empty())
+        std::cout << this->_float << "f" << std::endl;
+    else
+        std::cout << this->_exceptions[kFloat] << std::endl;
+    std::cout << "double: ";
+    if (this->_exceptions[kDouble].empty())
+        std::cout << this->_double << std::endl;
+    else
+        std::cout << this->_exceptions[kDouble] << std::endl;
+    return;
+}
+
+void    Convert::checkOverflow(std::string input) {
+    long double tmp;
+
+    sscanf(input.c_str(), "%Lf", &tmp);
+    if (tmp < 0 || tmp > 127 || this->_flag == kNan)
+        this->_exceptions[kChar] = "impossible";
+    else if (!isgraph(tmp)) // isgraph excludes space, since we want "displayable" chars
+        this->_exceptions[kChar] = "non-displayable";
+    if (tmp > INT_MAX || tmp < INT_MIN || this->_flag == kNan)
+        this->_exceptions[kInt] = "impossible";
+    if (this->_flag != kInf && this->_flag != kNan) {
+        if (tmp > __FLT_MAX__ || tmp < -(__FLT_MAX__))
+            this->_exceptions[kFloat] = "impossible";
+        if (tmp > __DBL_MAX__ || tmp < -(__DBL_MAX__))
+            this->_exceptions[kDouble] = "impossible";
+    }
+    return;
+}
+
+void    Convert::convertChar(void) {
+    this->_int = static_cast<int>(this->_char);
+    this->_float = static_cast<float>(this->_char);
+    this->_double = static_cast<double>(this->_char);
+    return;
+}
+
+void    Convert::convertDigit(void) {
+    if (this->_exceptions[kChar].empty())
+        this->_char = static_cast<char>(this->_double);
+    if (this->_exceptions[kInt].empty())
+        this->_int = static_cast<int>(this->_double);
+    if (this->_exceptions[kFloat].empty())
+        this->_float = static_cast<float>(this->_double);
     return;
 }
 
@@ -57,7 +139,7 @@ Type    Convert::detectType(std::string &input) {
         this->_flag = kNan;
         return kDouble;
     }
-    for (int i = 0; i < input.length(); i++) {
+    for (size_t i = 0; i < input.length(); i++) {
         if (isdigit(input[i]) && type != kDouble && type != kFloat)
             type = kInt;
         else if (input[i] == '.' && type == kInt)
@@ -67,40 +149,3 @@ Type    Convert::detectType(std::string &input) {
     }
     return type;
 }
-
-void    Convert::convertData(std::string &input) {
-    if (this->_dataType == kChar)
-        convertChar(this->_char);
-    else if (this->_flag == kNone)
-        convertDigit(input);
-    else
-        convertSpecial();
-    return;
-}
-
-void    Convert::convertChar(char c) {
-    this->_int = static_cast<int>(c);
-    this->_float = static_cast<float>(c);
-    this->_double = static_cast<double>(c);
-    return;
-}
-
-void    Convert::convertDigit(std::string &input) {
-    sscanf(input.c_str(), "%lf", &this->_double);
-    if (this->_double < 0 || this->_double > 127)
-        this->_exceptions[kChar] = "impossible";
-    else if (!isgraph(this->_double))
-        this->_exceptions[kChar] = "non-displayable";
-    else
-        this->_char = static_cast<char>(this->_double);
-    if (this->_double > INT_MAX || this->_double < INT_MIN)
-        this->_exceptions[kInt] = "impossible";
-    else
-        this->_int = static_cast<int>(this->_double);
-    if (this->_double > __FLT_MAX__ || this->_double < __FLT_MIN__ )
-        this->_exceptions[kFloat] = "impossible";
-    else
-        this->_float = static_cast<float>(this->_double);
-    return;
-}
-
--- cpp06/ex00/Convert.hpp
+++ cpp06/ex00/Convert.hpp
@@ -6,7 +6,7 @@
 /*   By: mjiam <mjiam@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/07 17:11:02 by mjiam         #+#    #+#                 */
-/*   Updated: 2020/09/08 18:05:04 by mjiam         ########   odam.nl         */
+/*   Updated: 2020/09/09 15:57:02 by mjiam         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 #include <cctype> // isgraph
 #include <cstdio> // scanf
-#include <cstdlib> // atoi
 #include <exception> // exceptions
 #include <iostream> // cout
 #include <climits> // min/max for data types
@@ -37,20 +36,20 @@ enum    Flag {
 
 class Convert {
 public:
-    Convert(std::string &input);
+    Convert(void);
+    Convert(Convert const &other);
+    Convert &operator=(Convert const &other);
     ~Convert(void);
 
-
+    void        convertData(std::string &input);
+    void        printData(void) const;
 
 private:
-    Convert(void);
+    void        checkOverflow(std::string input);
+    void        convertChar(void);
+    void        convertDigit(void);
     Type        detectType(std::string &input);
-    void        convertData(std::string &input);
-    void        convertChar(char c);
-    void        convertDigit(std::string &input);
-    void        convertSpecial(void);
-    void        printData(void) const;
-    // std::string _types[3];
+
     Type        _dataType;
     Flag        _flag;
     std::string _exceptions[4];
