93a752c0bd170e03b90a5b6ef750e3732d405846
mjiam <mjiam@student.codam.nl>
2020-09-17 16:27:45 +0200

    Moved private methods to protected

--- cpp04/ex03/AMateria.hpp
+++ cpp04/ex03/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: mjiam <mjiam@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/27 17:40:15 by mjiam         #+#    #+#                 */
-/*   Updated: 2020/09/17 16:13:45 by mjiam         ########   odam.nl         */
+/*   Updated: 2020/09/17 16:25:52 by mjiam         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,12 @@ public:
     virtual AMateria    *clone(void) const = 0;
     virtual void        use(ICharacter &target);
 
+protected:
+    void                setType(std::string const &type);
+    void                setXP(unsigned int n);
+
 private:
     AMateria(void);
-    void                _setType(std::string const &type);
-    void                _setXP(unsigned int n);
 
     unsigned int        _xp;
     std::string         _type;
