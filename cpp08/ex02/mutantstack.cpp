8f5e2bf845d54fd18a3faeacade84bf8c195b417
mjiam <mjiam@student.codam.nl>
2020-09-29 17:09:58 +0200

    Removed redundant code from cpp file

--- cpp08/ex02/mutantstack.cpp
+++ cpp08/ex02/mutantstack.cpp
@@ -6,57 +6,10 @@
 /*   By: mjiam <mjiam@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/21 14:15:43 by mjiam         #+#    #+#                 */
-/*   Updated: 2020/09/21 19:20:26 by mjiam         ########   odam.nl         */
+/*   Updated: 2020/09/29 17:09:22 by mjiam         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mutantstack.hpp"
 
-// template <class T, class Container>
-// MutantStack<T,Container>::MutantStack(void) {
-//     return;
-// }
-
-// template <class T, class Container>
-// MutantStack<T,Container>::MutantStack(MutantStack const &other)
-//         :   std::stack<T,Container>(other) {
-//     return;
-// }
-
-// template <class T, class Container>
-// MutantStack<T,Container>            &MutantStack<T,Container>::operator=(
-//         MutantStack const &other) {
-//     if (this != &other) {
-//         std::stack<T,Container>::operator=(other);
-//     }
-//     return *this;
-// }
-
-// template <class T, class Container>
-// MutantStack<T,Container>::~MutantStack(void) {
-//     return;
-// }
-
-// template <class T, class Container>
-// typename MutantStack<T,Container>::iterator  MutantStack<T,Container>::begin(
-//         void) {
-//     return this->begin();
-// }
-
-// template <class T, class Container>
-// typename MutantStack<T,Container>::reverse_iterator  MutantStack<T,Container>::rbegin(
-//         void) {
-//     return this->rbegin();
-// }
-
-// template <class T, class Container>
-// typename MutantStack<T,Container>::iterator  MutantStack<T,Container>::end(
-//         void) {
-//     return this->end();
-// }
-
-// template <class T, class Container>
-// typename MutantStack<T,Container>::reverse_iterator  MutantStack<T,Container>::rend(
-//         void) {
-//     return this->rend();
-// }
+// stupid file requirements because templates are instanciated inline
